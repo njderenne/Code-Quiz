5653efa6eb3bc09fc6fdebc46e0aa9029bde1040
In Code We Trust <njderenne@gmail.com>
2020-09-24 10:50:29 -0500

    updated css and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+var questionArray = [
+    {
+        q: "What is the difference between 'var' and 'const'?",
+        a1: "'Const' is reassignalbe but 'var' is not",
+        a2: "'Var' is reassignable but 'const' is not",
+        a3: "'Var' is for inside functions where 'const' is global",
+        a4: "There is no difference between them"
+    },
+    {
+        q: "What is the dif between 'var' and 'const'?",
+        a1: "'Const' is reassignalbe but 'var' is not",
+        a2: "'Var' s reassignable but 'const' is not",
+        a3: "'Var' i for inside functions where 'const' is global",
+        a4: "There isn't difference between them"
+    }
+];
+
+console.log(questionArray);
